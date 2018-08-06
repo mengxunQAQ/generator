b2862418c7f17010c361b59800d786f79add22a4
孟勋 <17191205035@189.cn>
2018-08-06 17:49:13 +0800

    fix

--- src/baidu-api.py
+++ src/baidu-api.py
@@ -15,49 +15,55 @@ cfg.read(r'../conf.ini')
 APP_ID = cfg.get('baidu', 'app_id')
 SECRET_KEY = cfg.get('baidu', 'secret_key')
 BASE_URL = cfg.get('baidu', 'base_url')
-FROM_LANG = cfg.get('baidu', 'fromLang')
-TO_LANG = cfg.get('baidu', 'toLang')
+FROM_LANG = cfg.get('language', 'fromLang')
+TO_LANG = cfg.get('language', 'toLang')
 
 
 class Query():
     def __get__(self, instance, owner):
-       return self.q
+       return self.query
 
     def __set__(self, instance, value):
         try:
-            self.q = sys.argv[1]
+            self.query = sys.argv[1]
         except:
             raise(ValueError, "Parameter error")
 
 
 class Request:
-    q = Query()
-    def __init__(self, q):
-        self.q = q
-        self.fromLang = 'en'
-        self.toLang = 'zh'
 
+    query = Query()
+    def __init__(self, query):
+
+        self.query = query
+        self.sign = self.build_sign()
+
+    @property
+    def url(self):
+        extend_url = "?q={0}&from={1}&to={2}&appid={3}&salt={4}&sign={5}".format(self.query, FROM_LANG, TO_LANG, APP_ID, self.salt, self.sign)
+        return BASE_URL + extend_url
+
+    def request(self):
+        return requests.get(url=self.url)
+
+    def response(self):
+        response = self.request()
+        return json.loads(response.text)
+
+    def output(self):
+        response = self.response()
+        sys.stdout.write('\033[0;31m查询结果：' + response['trans_result'][0]['dst'] + '\033[0m\n')
+
+    def build_sign(self):
+        self.salt = random.randint(32768, 65536)
+        self.sign = APP_ID + self.query + str(self.salt) + SECRET_KEY
         self.m2 = hashlib.md5()
         self.m2.update(self.sign.encode("utf-8"))
         self.sign = self.m2.hexdigest()
 
-    @property
-    def url(self):
-        extend_url = "?q={0}&from={1}&to={2}&appid={3}&salt={4}&sign={5}".format(self.q, self.fromLang, self.toLang, appid, self.salt, self.sign)
-        return base_url + extend_url
+        return self.sign
 
-    def request(self):
-        response = requests.get(url=self.url)
-        self.response = json.loads(response.text)
-        sys.stdout.write('查询结果：' + self.response['trans_result'][0]['dst'] + '\n')
-
-    def build_sign(self):
-
-        self.salt = random.randint(32768, 65536)
-        self.sign = APP_ID + self.q + str(self.salt) + SECRET_KEY
-
-        return
 
 if __name__ == '__main__':
     instance = Request(sys.argv)
-    instance.request()
+    instance.output()
